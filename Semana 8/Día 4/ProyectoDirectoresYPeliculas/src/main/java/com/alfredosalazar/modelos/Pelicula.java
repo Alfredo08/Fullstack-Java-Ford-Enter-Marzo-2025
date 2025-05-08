04c18cf4a78eae9b308bdb09bb615590c972bcea
Alfredo08 <alfredosv08@gmail.com>
2025-05-08 16:37:09 -0600

    Base de datos y Spring

---
+++ Semana 8/Día 4/ProyectoDirectoresYPeliculas/src/main/java/com/alfredosalazar/modelos/Pelicula.java
@@ -0,0 +1,118 @@
+package com.alfredosalazar.modelos;
+
+import java.util.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+
+@Entity
+@Table(name="peliculas")
+public class Pelicula {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String titulo;
+	
+	private String imagen;
+	
+	private String descripcion;
+	
+	private Long anio;
+	
+	private String director;
+	
+	@Temporal(TemporalType.TIMESTAMP)
+	@Column(name="fecha_creacion")
+	private Date fechaCreacion;
+	
+	@Temporal(TemporalType.TIMESTAMP)
+	@Column(name="fecha_actualizacion")
+	private Date fechaActualizacion;
+	
+	public Pelicula() {}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getImagen() {
+		return imagen;
+	}
+
+	public void setImagen(String imagen) {
+		this.imagen = imagen;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public String getDirector() {
+		return director;
+	}
+
+	public void setDirector(String director) {
+		this.director = director;
+	}
+
+	public Date getFechaCreacion() {
+		return fechaCreacion;
+	}
+
+	public void setFechaCreacion(Date fechaCreacion) {
+		this.fechaCreacion = fechaCreacion;
+	}
+
+	public Date getFechaActualizacion() {
+		return fechaActualizacion;
+	}
+
+	public void setFechaActualizacion(Date fechaActualizacion) {
+		this.fechaActualizacion = fechaActualizacion;
+	} 
+	
+	public Long getAnio() {
+		return anio;
+	}
+
+	public void setAnio(Long anio) {
+		this.anio = anio;
+	}
+
+	@PrePersist
+	protected void onCreate() {
+		this.fechaCreacion = new Date();
+		this.fechaActualizacion = new Date();
+	}
+	
+	@PreUpdate
+	protected void onUpdate() {
+		this.fechaActualizacion = new Date();
+	}
+}
---
+++ Semana 8/Día 4/ProyectoDirectoresYPeliculas/src/main/webapp/WEB-INF/peliculas.jsp
@@ -0,0 +1,31 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title> Películas y directores </title>
+		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+		<link href="css/estilos.css" rel="stylesheet">
+	</head>
+	<body>
+		<div class="container">
+			<div class="row">
+				<h1> Lista de películas </h1>
+			</div>
+			<div class="row">
+				<c:forEach items="${listaDePeliculas}" var="pelicula">
+					<div class="col-3 tarjeta-pelicula">
+						<h3> ${pelicula.titulo} </h3>
+						<img class="imagen-pelicula" src="${pelicula.imagen}" alt="${pelicula.titulo}" >
+						<p> Director: ${pelicula.director} Año: ${pelicula.anio} </p>
+						<p> ${pelicula.descripcion} </p>
+					</div>
+				</c:forEach>
+			</div>
+		</div>
+		
+		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+	</body>
+</html>
